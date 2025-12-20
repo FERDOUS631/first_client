1c3165f0093e74573041be7b0ef9b54fd51c6ea2
Ferdous Reza <ferdousreza631@gmail.com>
2025-12-20 23:29:39 +0600

    done

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CSRF_TRUSTED_ORIGINS= ["https://ferdousportfolio.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ['https://ferdousportfolio.up.railway.app']
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
